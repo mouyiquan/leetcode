c44b890c7e598b2517f01b82707ca1305f54e310
mango <mango.mou@hp.com>
2022-03-28 22:24:10 +0800

    589: N 叉树的前序遍历

--- src/java/problem589/Solution589.java
+++ src/java/problem589/Solution589.java
@@ -1,5 +1,8 @@
 package problem589;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author mango
  * @version 1.0
@@ -8,4 +11,19 @@ package problem589;
  * @date 2022/3/28 11:08
  */
 public class Solution589 {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        for (Node ch : root.children) {
+            helper(ch, res);
+        }
+    }
 }
