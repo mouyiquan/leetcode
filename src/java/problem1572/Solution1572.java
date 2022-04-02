98a5303fbe2cd6fc17469567e959e8cf139deb1c
mango <123456>
2022-04-02 10:03:20 +0800

    1572. 矩阵对角线元素的和

--- src/java/problem1572/Solution1572.java
+++ src/java/problem1572/Solution1572.java
@@ -8,4 +8,11 @@ package problem1572;
  * @date 2022/4/1 15:51
  */
 public class Solution1572 {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length, sum = 0, mid = n / 2;
+        for (int i = 0; i < n; ++i) {
+            sum += mat[i][i] + mat[i][n - 1 - i];
+        }
+        return sum - mat[mid][mid] * (n & 1);
+    }
 }
