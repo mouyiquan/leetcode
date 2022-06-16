43bb43c5b9c1f009102b951a072f6dc6d783f720
mango <123456>
2022-06-16 09:35:53 +0800

    142. 环形链表 II

---
+++ src/java/structure/Solution142.java
@@ -0,0 +1,53 @@
+package structure;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author mango
+ * @version 1.0
+ * @description 142. 环形链表 II
+ * @email
+ * @date 2022/6/16 9:22
+ */
+public class Solution142 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> visited = new HashSet<ListNode>();
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return pos;
+            } else {
+                visited.add(pos);
+            }
+            pos = pos.next;
+        }
+        return null;
+
+    }
+
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
